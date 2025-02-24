cd9bc75b20e36e0a2067795d6cb0aca4bfa2ecf4
Troy <me@troyhancock.com>
2025-02-24 00:15:21 -0800

    Remove unused import of ContactForm from contact page component

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import Head1 from "@/components/layouts/contact/h1"
-import { ContactForm } from "@/components/forms/contact-form"
 
 export default function page() {
   return (
